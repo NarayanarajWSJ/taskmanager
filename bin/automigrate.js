57c95bfedc3fe11e8801a44b76ec1b01cda4cec9
FirstFeet <narayanarajk23@gmail.com>
2018-05-20 11:42:06 +0530

    first commit

---
+++ bin/automigrate.js
@@ -0,0 +1,45 @@
+var path = require('path');
+
+var app = require(path.resolve(__dirname, '../server/server'));
+var ds = app.datasources.postgres;
+
+var Schemas = ["taskuser","group","groupusers","tasks"];
+
+// for (i = 0; i < Schemas.length; i++) { 
+//   console.log(Schemas[i] + ' Creating')
+//   ds.isActual(Schemas[i], function(err) {
+//     ds.autoupdate(Schemas[i], function(err) {
+//       if (err){ 
+//         console.log(err)
+//         throw err;
+//       }
+//     });
+//   });
+//   ds.disconnect();
+// }
+
+
+// for (i = 0; i < Schemas.length; i++) { 
+//   var res=null;
+
+//   ds.isActual(Schemas[i]);
+//   console.log('actual');
+//   res = ds.autoupdate(Schemas[i]);
+//   console.log('autoupdate');
+//   ds.disconnect();
+//   console.log('disconnect');
+//   while(res==null){
+    
+//   }
+//   console.log(res);
+// }
+
+ds.isActual("tasks", function(err) {
+  ds.autoupdate("tasks", function(err) {
+    if (err){ 
+      console.log(err)
+      throw err;
+    }
+    ds.disconnect();
+  });
+});
